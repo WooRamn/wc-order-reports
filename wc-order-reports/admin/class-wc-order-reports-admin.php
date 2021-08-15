bfa7957925d0c2df5176475438a4b1841cb08359
WooRamn <wooramn.plugin@gmail.com>
2021-08-15 21:42:46 +0530

    update plugin
    update on 15-8-21

---
+++ wc-order-reports/admin/class-wc-order-reports-admin.php
@@ -0,0 +1,168 @@
+<?php
+
+/**
+ * The admin-specific functionality of the plugin.
+ *
+ * @link       
+ * @since      1.0.0
+ *
+ * @package    Wc_Order_Reports
+ * @subpackage Wc_Order_Reports/admin
+ */
+
+/**
+ * The admin-specific functionality of the plugin.
+ *
+ * Defines the plugin name, version, and two examples hooks for how to
+ * enqueue the admin-specific stylesheet and JavaScript.
+ *
+ * @package    Wc_Order_Reports
+ * @subpackage Wc_Order_Reports/admin
+ * @author     Wooramn <wcorder.reports@gmail.com>
+ */
+if(!defined('ABSPATH')){
+	exit; // Exit if accessed directly
+}
+
+if(!class_exists('Wc_Order_Reports_Admin')):
+class Wc_Order_Reports_Admin{
+
+	/**
+	 * The ID of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      string    $plugin_name    The ID of this plugin.
+	 */
+	private $plugin_name;
+
+	/**
+	 * The version of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      string    $version    The current version of this plugin.
+	 */
+	private $version;
+
+	/**
+	 * Initialize the class and set its properties.
+	 *
+	 * @since    1.0.0
+	 * @param      string    $plugin_name       The name of this plugin.
+	 * @param      string    $version    The version of this plugin.
+	 */
+	protected $wc_order_url;
+	protected $screen_id;
+	public function __construct( $plugin_name, $version ) {
+		$this->plugin_name = $plugin_name;
+		$this->version = $version;
+		$this->wc_order_url = "admin.php?page=".$plugin_name;
+		$this->screen_id = isset($_GET['page'])?$_GET['page']:"";
+		add_action( 'wc_order_header', array($this,'start_page_contener'), 10);
+		add_action( 'wc_order_header', array($this,'add_page_tabs'), 20);
+		add_action( 'wc_order_footer', array($this,'end_page_contener'), 10);
+	}
+
+	/**
+	 * Register the stylesheets for the admin area.
+	 *
+	 * @since    1.0.0
+	 */
+	public function wc_order_enqueue_styles() {
+		if(str_contains($this->screen_id,"wc-order-reports")){
+			wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/wc-order-reports-admin.css', array(), $this->version, 'all' );
+		}
+	}
+
+	/**
+	 * Register the JavaScript for the admin area.
+	 *
+	 * @since    1.0.0
+	 */
+	public function wc_order_enqueue_scripts() {		
+		if(str_contains($this->screen_id,"wc-order-reports")){
+			wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/wc-order-reports-admin.js', array( 'jquery' ), $this->version, false );
+		}
+	}
+	/**
+	 * Add Menu for the admin area.	 *
+	 * @since    1.0.0
+	 */
+	public function wc_order_admin_menu(){
+		add_menu_page(
+      'WO Reports', 'WC Order Reports', 'manage_options', "wc-order-reports", array($this, 'show_page'), plugin_dir_url(__FILE__) . 'images/logo-icon.png', 56
+  	);
+  	add_submenu_page('wc-order-reports', 'Order Overview', 'Order Overview', 'manage_options', 'wc-order-reports' );
+  	add_submenu_page('wc-order-reports', 'Download', 'Download', 'manage_options', 'wc-order-reports-download', array($this, 'show_page'));
+	}
+	public function start_page_contener(){
+		echo '<div class="wrap wor-main-contener">';
+	}
+	public function end_page_contener(){
+		echo '</div>';
+	}
+ /* add active tab class */
+  protected function is_active_tabs($tab_name=""){
+    if($tab_name!="" && isset($_GET['page']) && $_GET['page'] == $tab_name){
+      return "tab-current";
+    }
+  }
+	public function add_page_tabs(){
+		?>
+		<div class="tabs wor-tabs">
+			<nav>
+				<ul>
+					<li class="<?php echo $this->is_active_tabs("wc-order-reports"); ?>"><a href="<?php echo $this->wc_order_url; ?>" class="icon icon-home"><span>Order Overview</span></a></li>
+					<li class="<?php echo $this->is_active_tabs('wc-order-reports-download'); ?>"><a href="<?php echo $this->wc_order_url.'-download'; ?>" class="icon icon-download"><span>Download Reports</span></a></li>
+				</ul>
+			</nav>
+		</div>
+		<?php
+	}
+	/**
+	 * Load page for the admin area.	 *
+	 * @since    1.0.0
+	 */
+	public function show_page() {
+		$get_action = "wc_order_reports";
+   	if(isset($_GET['page'])) {
+      $get_action = str_replace("-", "_", $_GET['page']);
+    }
+     //echo $get_action ='get_'.$get_action.'_page';
+    if(method_exists($this, $get_action)){
+      $this->$get_action();
+    }
+  }
+
+  /**
+	 * Load dashboard page for the admin area.	 *
+	 * @since    1.0.0
+	 */
+  public function wc_order_reports(){
+  	require_once( 'partials/class-wc-order-reports.php');
+  	new WC_Order_Dashboard();
+  }
+  /**
+	 * Load dashboard page for the admin area.	 *
+	 * @since    1.0.0
+	 */
+  public function wc_order_reports_download(){
+  	require_once( 'partials/class-wc-order-reports-download.php');
+  	new WC_Order_Download();
+  }
+	/**
+	 * Add for language translations.	 *
+	 * @since    1.0.0
+	 */
+	public function wc_order_translations(){
+		$locale ="";
+		if ( function_exists( 'determine_locale' ) ) { // WP5.0+
+			$locale = determine_locale();
+		} else {
+			$locale = is_admin() && function_exists( 'get_user_locale' ) ? get_user_locale() : get_locale();
+		}
+		load_plugin_textdomain( 'wc-order-reports', false, dirname( plugin_basename(__FILE__) ) . '/languages' );
+	}
+} //Wc_Order_Reports_Admin
+endif; // class_exists
---
+++ wc-order-reports/wc-order-reports.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              
+ * @since             1.0.0
+ * @package           Wc_Order_Reports
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Order Reports for WooCommerce
+ * Plugin URI:        https://wordpress.org/plugins/wc-order-reports
+ * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Version:           1.0.0
+ * Requires at least: 4.4.0
+ * Tested up to: 5.8
+ * WC requires at least:    4.0
+ * WC tested up to:         5.5.2
+ * Author:            Ramnik Chavda
+ * Author URI:        
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       wc-order-reports
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * Currently plugin version.
+ * Start at version 1.0.0
+ */
+
+define( 'Wc_Order_Reports_VERSION', '1.0.0' );
+if ( ! defined( 'WC_ORDER_REPOSTS_PLUGIN_DIR' ) ) {
+    define( 'WC_ORDER_REPOSTS_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+}
+if ( ! defined( 'WC_ORDER_REPOSTS_PLUGIN' ) ) {
+    define( 'WC_ORDER_REPOSTS_PLUGIN', basename(__DIR__) );
+}
+if ( ! defined( 'WC_ORDER_REPOSTS_PLUGIN_URL' ) ) {
+    define( 'WC_ORDER_REPOSTS_PLUGIN_URL', plugins_url() . '/'.WC_ORDER_REPOSTS_PLUGIN );
+}
+/**
+ * The code that runs during plugin activation.
+ * This action is documented in includes/class-wc-order-reports-activator.php
+ */
+function activate_wc_order_reports() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-wc-order-reports-activator.php';
+	Wc_Order_Reports_Activator::activate();
+}
+
+/**
+ * The code that runs during plugin deactivation.
+ * This action is documented in includes/class-wc-order-reports-deactivator.php
+ */
+function deactivate_wc_order_reports() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-wc-order-reports-deactivator.php';
+	Wc_Order_Reports_Deactivator::deactivate();
+}
+
+register_activation_hook( __FILE__, 'activate_Wc_Order_Reports' );
+register_deactivation_hook( __FILE__, 'deactivate_Wc_Order_Reports' );
+
+/**
+ * The core plugin class that is used to define internationalization,
+ * admin-specific hooks, and public-facing site hooks.
+ */
+require plugin_dir_path( __FILE__ ) . 'includes/class-wc-order-reports.php';
+
+/**
+ * Begins execution of the plugin.
+ *
+ * Since everything within the plugin is registered via hooks,
+ * then kicking off the plugin from this point in the file does
+ * not affect the page life cycle.
+ *
+ * @since    1.0.0
+ */
+function run_wc_order_reports() {
+
+	$plugin = new Wc_Order_Reports();
+	$plugin->run();
+
+}
+run_wc_order_reports();
